5b0682562cd2176c6b28947fd4ef916b9d13a315
Martinits <mamartinitramam@163.com>
2021-11-03 00:09:05 +0800

    pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// implementation of vmprint
+static void
+vmprint_recur(pagetable_t pagetable, int predots)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(! (pte & PTE_V)) continue;
+
+    for(int j = 0; j < predots-1; j++) printf(".. ");
+    uint64 child = PTE2PA(pte);
+    printf("..%d: pte %p pa %p\n", i, pte, child);
+    if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+      vmprint_recur((pagetable_t)child, predots+1);
+    }
+  }
+}
+
+// print specific pagetable
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_recur(pagetable, 1);
+}
